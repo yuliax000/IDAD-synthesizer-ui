b984c542c0b04843fdd050986b4b0f4e2b90fecc
yuliax000 <ArthyerX@gmail.com>
2025-09-03 20:30:22 +1000

    xycontroller set (cxcy NaN)

--- testXyController/xyController.js
+++ testXyController/xyController.js
@@ -8,7 +8,7 @@ xyControllers.forEach((xyBox) => {
     svgCTM = xyBox.querySelector(".xySVGEl").getScreenCTM();
   });
 
-  let markerXPos,
+  let markerXPos = 3,
     markerYPos = 3;
   let dragging = false;
 
@@ -17,4 +17,38 @@ xyControllers.forEach((xyBox) => {
     document.addEventListener("mousemove", xyDragging);
     document.addEventListener("mouseup", endXYDrag);
   }
+  function xyDragging(e) {
+    if (!dragging) {
+      window.requestAnimationFrame(() => {
+        markerXPos = clamp((e.clientX - svgCTM.e) / svgCTM.a, 0, 1000);
+        markerYPos = clamp((e.clientY - svgCTM.f) / svgCTM.d, 0, 1000);
+        xyPosMarker.setAttribute("cx", markerXPos);
+        xyPosMarker.setAttribute("cy", markerYPos);
+
+        newXValue(markerXPos);
+        newYValue(markerYPos);
+        dragging = false;
+      });
+      dragging = true;
+    }
+  }
+
+  function endXYDrag() {
+    document.body.classList.remove("drag-active");
+    document.removeEventListener("mousemove", xyDragging);
+    document.removeEventListener("mouseup", endXYDrag);
+  }
+
+  xyBox.addEventListener("mousedown", startxyDrag);
+  xyBox.addEventListener("mousedown", xyDragging);
 });
+// function newXValue(value) {
+//   xOutputText.textContent = value;
+// }
+// function newYValue(value) {
+//   yOutputText.textContent = value;
+// }
+
+function clamp(value, min, max) {
+  return Math.min(Math.max(value.min), max);
+}
