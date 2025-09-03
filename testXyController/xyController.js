e962107bb5f08db275a14a628dd80a08bcadbb45
yuliax000 <ArthyerX@gmail.com>
2025-09-03 18:55:11 +1000

    change style to make xyposMarker at the center

--- testXyController/xyController.js
+++ testXyController/xyController.js
@@ -1,20 +1,20 @@
 let xOutputText = document.getElementById("xPosOutput");
 let yOutputText = document.getElementById("yPosOutput");
-let xyControllers = Array.from(document.getElementByClassName("xyControlBox"));
+let xyControllers = Array.from(document.getElementsByClassName("xyControlBox"));
 xyControllers.forEach((xyBox) => {
   let xyPosMarker = xyBox.querySelector(".xyPosMarker");
   let svgCTM = xyBox.querySelector(".xySVGEl").getScreenCTM();
   window.addEventListener("resize", () => {
     svgCTM = xyBox.querySelector(".xySVGEl").getScreenCTM();
   });
+
+  let markerXPos,
+    markerYPos = 3;
+  let dragging = false;
+
+  function startxyDrag() {
+    document.body.classList.add("drag-active");
+    document.addEventListener("mousemove", xyDragging);
+    document.addEventListener("mouseup", endXYDrag);
+  }
 });
-
-let markerXPos,
-  markerYPos = 3;
-let dragging = false;
-
-function startxyDrag() {
-  document.body.classList.add("drag-active");
-  document.addEventListener("mousemove", xyDragging);
-  document;
-}
