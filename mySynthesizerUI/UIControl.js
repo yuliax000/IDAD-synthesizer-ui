9b005dd17a95a35d456348f93cbc05544ab5a8f2
yuliax000 <ArthyerX@gmail.com>
2025-09-09 14:21:18 +1000

    add icons for pause and reset

--- mySynthesizerUI/UIControl.js
+++ mySynthesizerUI/UIControl.js
@@ -1,12 +1,20 @@
 const xyPad = document.getElementById("xyPad");
 
-xyPad.addEventListener("click", (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-});
-
 const volumeSlider = document.getElementById("volumeSlider");
 volumeSlider.addEventListener("input", (e) => {
   gainNode.gain.value = e.target.value;
   console.log("volumeNow", gainNode.gain.value);
+});
+
+// apply pause button and reset button
+const pauseBtn = document.getElementById("pauseBtn");
+pauseBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (audioCtx.state === "running") {
+    audioCtx.suspend();
+    console.log("paused");
+  } else if (audioCtx.state === "suspended") {
+    audioCtx.resume();
+    console.log("Resumed");
+  }
 });
