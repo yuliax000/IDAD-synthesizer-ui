588458cd7837878835a2aa4a68c6d00c2c14c541
yuliax000 <ArthyerX@gmail.com>
2025-09-27 21:00:45 +1000

    add single ripple

--- visualui.js
+++ visualui.js
@@ -1,1 +1,24 @@
+const svg = document.querySelector(".xySVGEl");
 
+svg.addEventListener("click", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  // create circle
+  const circle = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
+  circle.setAttribute("cx", x);
+  circle.setAttribute("cy", y);
+  circle.setAttribute("r", 0);
+  circle.classList.add("ripple-circle");
+
+  svg.appendChild(circle);
+
+  // delete after animation finished
+
+  circle.addEventListener("animationend", () => {
+    circle.remove();
+  });
+});
